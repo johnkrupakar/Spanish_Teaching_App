f293f3a18b1571e57fdec17578bbe00582e5ca8a
johnkrupakar <john245461@gmail.com>
2022-05-13 19:00:16 +0530

    Initial commit

---
+++ app/src/main/java/com/masterandroid/spanishteacher/MainActivity.java
@@ -0,0 +1,29 @@
+package com.masterandroid.spanishteacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SayTheColor(View view){
+        Button buttonClicked = (Button) view;
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(
+                this,
+                getResources().getIdentifier(buttonClicked.getTag().toString(),
+                "raw",
+                getPackageName()));
+
+        mediaPlayer.start();
+    }
+}
